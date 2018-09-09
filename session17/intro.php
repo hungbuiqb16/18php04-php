93974d3bccfbe16b23be95a1f90456abf10f3c36
hungbuiqb16 <37810082+hungbuiqb16@users.noreply.github.com>
2018-09-09 09:01:40 +0700

    update code

--- session17/intro.php
+++ session17/intro.php
@@ -1,6 +1,16 @@
 <?php
-  function mySum($numberA, $numberB){
+  function mySum($numberA, $numberB) {
   	return $numberA + $numberB;
   }
+  echo "<br>";
   echo mySum(5,7);
+  echo "<br>";
+  $total = mySum(5,8);
+  if ($total % 2 == 0) {
+  	echo "Tong la so chan";
+  } elseif ($total % 3 == 0) {
+  	echo "Tong la so le va chia het cho 3";
+  } else {
+  	echo "Tong la so le va khong chia het cho 3";
+  }
 ?>
